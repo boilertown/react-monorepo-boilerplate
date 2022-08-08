fec42b22cdef46a6bbd7e406eb653bc8ca532b66
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-08-08 22:50:25 +0700

    feat: home page content

--- apps/dashboard/src/pages/home/HomePage.tsx
+++ apps/dashboard/src/pages/home/HomePage.tsx
@@ -1,11 +1,22 @@
+import { Button } from '@boilertowns-example/ui';
+import { useNavigate } from 'react-router-dom';
 import { SingleColumnLayout } from 'components/Layouts';
 import { Typography } from 'components/Typography';
 
 const HomePage = () => {
+	const navigate = useNavigate();
+
 	return (
 		<SingleColumnLayout
 			renderHeader={null}
-			renderMain={<Typography.Title>Login</Typography.Title>}
+			renderMain={
+				<>
+					<Typography.Title>Home</Typography.Title>
+					<div>
+						<Button onClick={() => navigate('/login')}>Login</Button>
+					</div>
+				</>
+			}
 			renderFooter={null}
 		/>
 	);
